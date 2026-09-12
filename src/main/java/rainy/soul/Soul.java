9fe72f862b74fb0b8f07d21fbdaedf483cedeefe
Abdaleam <lalaabdalaem@gmail.com>
2026-09-13 02:09:15 +0300

    fixed the repo now

--- src/main/java/rainy/soul/Soul.java
+++ src/main/java/rainy/soul/Soul.java
@@ -7,7 +7,7 @@ import org.slf4j.LoggerFactory;
 import rainy.soul.item.SoulItems;
 
 public class Soul implements ModInitializer {
-	public static final String MOD_ID = "milk";
+	public static final String MOD_ID = "soul";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
